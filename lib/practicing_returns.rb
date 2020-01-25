c5d6d24e630a8167b34d0468006e10e301b5df99
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-25 04:09:44 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -9,4 +9,12 @@ def hello(array)
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+    def hello(array)
+      i = 0
+      collection = []
+      while i < array.length
+        collection << yield(array[i])
+        i += 1
+      end
+      collection
+    end
